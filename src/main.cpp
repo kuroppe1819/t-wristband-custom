06380778369e0a5a05f49f3496e4709a62e059d1
kuroppe <kuroppems@gmail.com>
2020-07-12 22:50:29 +0900

    Show time on the LCD

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,70 @@
 #include <SPI.h>
 #include <TFT_eSPI.h>  // Graphics and font library for ST7735 driver chip
 #include <Wire.h>
+#include <pcf8563.h>
 
+#define I2C_SDA_PIN 21
+#define I2C_SCL_PIN 22
 #define TP_PIN_PIN 33
 #define TP_PWR_PIN 25
 #define LED_PIN 4
 
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
+PCF8563_Class rtc;
 
-uint8_t func_select = 0;
 boolean pressed = false;
+uint8_t func_select = 0;
 uint8_t MAX_SELECT_MODE_COUNT = 2;
+RTC_Date datetime;
+
+void setupRTC() {
+  rtc.begin(Wire);
+  rtc.check();  // Check if the RTC clock matches, if not, use compile time
+
+  datetime = rtc.getDateTime();
+}
 
 void setup() {
   Serial.begin(115200);
 
   tft.init();
-  tft.setRotation(1);
   tft.setSwapBytes(true);
+
+  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
+  Wire.setClock(400000);
+
+  setupRTC();
 
   pinMode(TP_PIN_PIN, INPUT);
   pinMode(TP_PWR_PIN, PULLUP);  //! Must be set to pull-up output mode in order
                                 //! to wake up in deep sleep mode
   digitalWrite(TP_PWR_PIN, HIGH);
+}
+
+void showTime() {
+  datetime = rtc.getDateTime();
+
+  // show hour
+  tft.setRotation(0);  // button exist down direction
+  if (datetime.hour < 10) {
+    tft.drawString("0" + String(datetime.hour), 9, 16, 7);
+  } else {
+    tft.drawString(String(datetime.hour), 9, 16, 7);
+  }
+  tft.setTextColor(TFT_SKYBLUE, TFT_BLACK);
+
+  // show colon
+  tft.setRotation(1);  // horizonal
+  tft.drawString(":", tft.width() / 2 - 8, tft.height() / 2 - 24, 7);
+
+  // show minute
+  tft.setRotation(0);  // button exist down direction
+  if (datetime.minute < 10) {
+    tft.drawString("0" + String(datetime.minute), 10, 90, 7);
+  } else {
+    tft.drawString(String(datetime.minute), 10, 90, 7);
+  }
+  tft.setTextColor(TFT_SKYBLUE, TFT_BLACK);
 }
 
 void SleepMode() {
@@ -51,16 +93,16 @@ void loop() {
 
   switch (func_select) {
     case 0:
-      tft.setTextColor(TFT_SKYBLUE, TFT_BLACK);
-      tft.setTextDatum(MC_DATUM);
-      tft.drawString("HOGE", tft.width() / 2, tft.height() / 2, 4);
+      showTime();
       break;
     case 1:
+      tft.setRotation(2);
       tft.setTextColor(TFT_SKYBLUE, TFT_BLACK);
       tft.setTextDatum(MC_DATUM);
       tft.drawString("PIYO", tft.width() / 2, tft.height() / 2, 4);
       break;
     case 2:
+      tft.setRotation(0);
       tft.setTextColor(TFT_GREEN, TFT_BLACK);
       tft.setTextDatum(MC_DATUM);
       tft.drawString("Press again to wake up", tft.width() / 2,
